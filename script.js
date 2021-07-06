70cfe17553a309d76496a86b87cf559ebbc4d75f
sofia durkan <sofiadurkan@gmail.com>
2021-07-06 00:13:35 -0500

    completed

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
 let mySong = document.getElementById("mySong");
-        let icon = document.getElementById("icon");
-         
-        icon.onclick = function(){
-            if(mySong.paused){
-                mySong.play();
-                icon.src = "./images/pause.png"
+let icon = document.getElementById("icon");
 
-            }else{
-                mySong.pause();
-                icon.src = "./images/play.png"
-            }
-            
+icon.onclick = function () {
+  if (mySong.paused) {
+    mySong.play();
+    icon.src = "./images/pause.png";
+  } else {
+    mySong.pause();
+    icon.src = "./images/play.png";
+  }
+};
 
-        }
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+}
